0d74f6ff690653e570c57fdff6224b259b8f7455
mnosumon <mdnuruddin378350@gmail.com>
2025-02-24 11:09:14 +0600

    Model create done

--- app/Models/employee.php
+++ app/Models/employee.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class employee extends Model
 {
-    //
+    protected $fillable = [
+        'employee_id',
+        'full_name',
+        'contact_no',
+        'joining_date',
+        'gender',
+        'Employee_status',
+        'marital_status',
+        'religion',
+        'date_of_birth',
+        'user_name',
+        'department',
+        'designation',
+        'email',
+        'supervisor_team_lead',
+        'blood_group',
+        'branch_wing',
+        'finger_device_id',
+        'attendance_policy',
+        'rf_id',
+        'leave_approver',
+    ];
 }
